4a44a913072d17f80effd2ef68e0b4251d3ab733
ZJH17778383963 <992538079@qq.com>
2022-11-17 22:05:09 +0800

    fix空格转换

--- src/main/java/com/zklt/parsing/handler/MessageAction.java
+++ src/main/java/com/zklt/parsing/handler/MessageAction.java
@@ -2,9 +2,7 @@ package com.zklt.parsing.handler;
 
 import com.zklt.parsing.model.entity.HandlerMessage;
 import com.zklt.parsing.model.entity.Message;
-import org.yaml.snakeyaml.introspector.PropertyUtils;
 
-import java.beans.PropertyDescriptor;
 import java.io.*;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -43,14 +41,16 @@ public interface MessageAction<T extends Message> {
         //把多个空格转换为一个空格
         data = data.trim().replaceAll(" +", " ");
         String[] dates = data.split(" ");
-        if (dates.length!=clazz.getFields().length){
+        Field[] files = clazz.getFields();
+        if (dates.length!=files.length){
             return null;
         }
         HandlerMessage<T> resMessage = new HandlerMessage<>();
         T body = clazz.newInstance();
-        for (int i= 0;i<dates.length;i++){
-            clazz.getFields()[i].setAccessible(true);
-            clazz.getFields()[i].set(body,dates[i]);
+        for (int i= 0;i<files.length;i++){
+            Field field = files[i];
+            field.setAccessible(true);
+            field.set(body,dates[i]);
         }
         resMessage.setMessage(body);
         return resMessage;
