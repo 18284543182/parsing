5dbc9a5b4769adc5262daa2b20f7d338af76b56a
伍汭 <1490208693@qq.com>
2022-11-22 00:04:29 +0800

    dgd解析修改

--- src/main/java/com/zklt/parsing/handler/action/SpaceWeatherMonitoringDgdAction.java
+++ src/main/java/com/zklt/parsing/handler/action/SpaceWeatherMonitoringDgdAction.java
@@ -5,6 +5,16 @@ import com.zklt.parsing.model.entity.HandlerMessage;
 import com.zklt.parsing.model.entity.SpaceWeatherMonitoringDgd;
 import com.zklt.parsing.model.enums.Mapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author wurui
@@ -18,5 +28,30 @@ public class SpaceWeatherMonitoringDgdAction implements MessageAction<SpaceWeath
     public Object doAction(HandlerMessage<SpaceWeatherMonitoringDgd> message) {
         message.getMessage().setPath(message.getSrcFilePath());
         return message.getMessage();
+    }
+
+    @Override
+    public List<String> readFile(File file) {
+        List<String> result = new ArrayList<>();
+        if(file.exists())
+        {
+            try (InputStreamReader input = new InputStreamReader(Files.newInputStream(file.toPath()), StandardCharsets.UTF_8); BufferedReader br = new BufferedReader(input)){
+                String line = null;
+                //避免数据中间出现空格所以获取数据总行数进行遍历
+                long countLine = Files.lines(file.toPath()).count();
+                for (int i = 0;i<countLine+1;i++){
+                    line = br.readLine();
+                    if (!StringUtils.isEmpty(line) &&!line.startsWith("#")&&!line.startsWith(":")) {
+                        String linenew=  line.replace("-"," -");
+                        result.add(linenew);
+                    }
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
     }
 }
