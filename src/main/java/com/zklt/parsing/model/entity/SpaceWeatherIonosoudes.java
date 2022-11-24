df4938b1505612b37e52cd3787f435c171ec522b
伍汭 <1490208693@qq.com>
2022-11-24 21:46:34 +0800

    修改

---
+++ src/main/java/com/zklt/parsing/model/entity/SpaceWeatherIonosoudes.java
@@ -0,0 +1,29 @@
+package com.zklt.parsing.model.entity;
+
+import lombok.Data;
+
+/**
+ * @author wurui
+ * @date 2022/11/24 21:44
+ * @Description:
+ */
+@Data
+public class SpaceWeatherIonosoudes extends Message{
+
+    /**
+     * 台站代码
+     */
+    public String stationCode;
+
+    public String date;
+
+    public String time;
+
+    public String fof2;
+
+    public String m3000;
+
+    public String fmin;
+
+    public String value;
+}
