fc77952a6aac07b06e33686c41fdbca3256f809c
伍汭 <1490208693@qq.com>
2022-11-24 21:37:36 +0800

    修改冲突

--- src/main/java/com/zklt/parsing/handler/action/SpaceweatherIonoSoudeAction.java
+++ src/main/java/com/zklt/parsing/handler/action/SpaceweatherIonoSoudeAction.java
@@ -2,42 +2,61 @@ package com.zklt.parsing.handler.action;
 
 import com.zklt.parsing.handler.MessageAction;
 import com.zklt.parsing.model.entity.HandlerMessage;
-import com.zklt.parsing.model.entity.SpaceWeatherS4Uhfband;
+import com.zklt.parsing.model.entity.SpaceWeatherIonosoude;
+import com.zklt.parsing.model.entity.SpaceWeatherRadioShidao;
 import com.zklt.parsing.model.enums.Mapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-import java.lang.reflect.Field;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * @author xiejian
- * @date 2022/11/24 10:26
+ * @author wurui
+ * @date 2022/11/24 20:38
  * @Description:
  */
 @Service
-@Mapper(type = "SpaceweatherIonoSoude", getAction = SpaceWeatherS4Uhfband.class)
-public class SpaceweatherIonoSoudeAction implements MessageAction<SpaceWeatherS4Uhfband> {
+@Mapper(type = "SpaceWeatherIonosoude", getAction = SpaceWeatherIonosoude.class)
+public class SpaceWeatherIonosoudeAction implements MessageAction<SpaceWeatherIonosoude> {
     @Override
-    public Object doAction(HandlerMessage<SpaceWeatherS4Uhfband> message) {
+    public Object doAction(HandlerMessage<SpaceWeatherIonosoude> message) {
         message.getMessage().setPath(message.getSrcFilePath());
         return message.getMessage();
     }
 
     @Override
-    public  HandlerMessage<SpaceWeatherS4Uhfband> decode(String data, Class<SpaceWeatherS4Uhfband> clazz) throws InstantiationException, IllegalAccessException {
-        data = data.trim().replaceAll(" +", " ");
-        String[] dates = data.split("\\s+");
-        Field[] files = clazz.getFields();
-        if (dates.length!=files.length){
-            return null;
+    public List<String> readFile(File file) {
+        List<String> result = new ArrayList<>();
+        if(file.exists())
+        {
+            try (InputStreamReader input = new InputStreamReader(Files.newInputStream(file.toPath()), StandardCharsets.UTF_8); BufferedReader br = new BufferedReader(input)){
+                String line = null;
+                //避免数据中间出现空格所以获取数据总行数进行遍历
+                long countLine = Files.lines(file.toPath()).count();
+                String date="";
+                String time="";
+                for (int i = 0;i<countLine+1;i++){
+                    line = br.readLine();
+                    if (!StringUtils.isEmpty(line)) {
+                        line=line.replaceAll("\t"," ");
+                        line=line.replace("/","-");
+                        result.add(line);
+
+                    }
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        HandlerMessage<SpaceWeatherS4Uhfband> resMessage = new HandlerMessage<>();
-        SpaceWeatherS4Uhfband body = clazz.newInstance();
-        for (int i= 0;i<files.length;i++){
-            Field field = files[i];
-            field.setAccessible(true);
-            field.set(body,dates[i]);
-        }
-        resMessage.setMessage(body);
-        return resMessage;
+
+        return result;
     }
 }
