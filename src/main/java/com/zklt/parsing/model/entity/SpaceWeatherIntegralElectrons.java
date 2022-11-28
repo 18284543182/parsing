aa7ca2f1a4f650b824e774c9fdfdf18f87ed68cf
伍汭 <1490208693@qq.com>
2022-11-28 22:35:15 +0800

    磁层粒子辐射 电子 质子通量检测数据

---
+++ src/main/java/com/zklt/parsing/model/entity/SpaceWeatherIntegralElectrons.java
@@ -0,0 +1,13 @@
+package com.zklt.parsing.model.entity;
+
+import lombok.Data;
+
+/**
+ * @author wurui
+ * @date 2022/11/28 21:59
+ * @Description: 磁层粒子辐射 电子通量检测数据
+ */
+@Data
+public class SpaceWeatherIntegralElectrons extends Message{
+
+}
---
+++ src/main/java/com/zklt/parsing/model/entity/SpaceWeatherIntegralProtons.java
@@ -0,0 +1,9 @@
+package com.zklt.parsing.model.entity;
+
+/**
+ * @author wurui
+ * @date 2022/11/28 22:02
+ * @Description: 磁层粒子辐射 质子通量检测数据
+ */
+public class SpaceWeatherIntegralProtons extends Message{
+}
