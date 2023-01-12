d1776f9f71da86f3f81319a5dd3048bf19e64680
伍汭 <1490208693@qq.com>
2023-01-12 19:15:10 +0800

    地磁K指数

--- src/main/java/com/zklt/parsing/handler/action/SpaceWeatherMonitoringDgdAction.java
+++ src/main/java/com/zklt/parsing/handler/action/SpaceWeatherMonitoringDgdAction.java
@@ -13,8 +13,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author wurui
@@ -39,11 +38,82 @@ public class SpaceWeatherMonitoringDgdAction implements MessageAction<SpaceWeath
                 String line = null;
                 //避免数据中间出现空格所以获取数据总行数进行遍历
                 long countLine = Files.lines(file.toPath()).count();
+                String stationcode1="";
+                String stationcode2="";
+                String stationcode3="";
+                List<String> cachestr=new ArrayList<>();
+
+                List<String> cachestrtrue=new ArrayList<>();
                 for (int i = 0;i<countLine+1;i++){
                     line = br.readLine();
+                    cachestr.add(line);
                     if (!StringUtils.isEmpty(line) &&!line.startsWith("#")&&!line.startsWith(":")) {
+                        String stationcodes=cachestr.get(i-2);
+                        String[] stars=stationcodes.split(" ");
+                        for (int j=0;j<stars.length;j++){
+                            String str=stars[j];
+                            if (str != null && str.length() != 0&&!str.equals(" ")&&!str.contains("-")&&!str.contains("#")){
+                                cachestrtrue.add(str);
+                            }
+                        }
+
                         String linenew=  line.replace("-"," -");
-                        result.add(linenew);
+
+                        String[] datasnew=linenew.split(" ");
+
+                        List<String> datas=new ArrayList<>();
+
+                        for (int b=0;b<datasnew.length;b++){
+                            String str=datasnew[b];
+                            if (str != null && str.length() != 0&&!str.equals(" ")){
+                                datas.add(str);
+                            }
+                        }
+                        String year=datas.get(0);
+                        String month=datas.get(1);
+                        String day=datas.get(2);
+                        int h1=0;
+                        int h2=0;
+                        int h3=0;
+
+                        for (int a=0;a<datas.size();a++){
+                            if (a>=4&&a<=11){
+                                String hour="";
+                                if (h1<10) {
+                                    hour="0"+h1+":00:00";
+                                }else {
+                                    hour=h1+":00:00";
+                                }
+                                String returnstr=year+" "+month+" "+day+" "+hour+" "+cachestrtrue.get(0)+" "+datas.get(a);
+                                result.add(returnstr);
+                                h1=h1+3;
+                            }
+                            if (a>=13&&a<=20){
+                                String hour="";
+                                if (h2<10) {
+                                    hour="0"+h2+":00:00";
+                                }else {
+                                    hour=h2+":00:00";
+                                }
+                                String returnstr=year+" "+month+" "+day+" "+hour+" "+cachestrtrue.get(1)+" "+datas.get(a);
+                                result.add(returnstr);
+                                h2=h2+3;
+                            }
+
+                            if (a>=22&&a<=29){
+                                String hour="";
+                                if (h3<10) {
+                                    hour="0"+h3+":00:00";
+                                }else {
+                                    hour=h3+":00:00";
+                                }
+                                String returnstr=year+" "+month+" "+day+" "+hour+" "+cachestrtrue.get(2)+" "+datas.get(a);
+                                result.add(returnstr);
+                                h3=h3+3;
+                            }
+
+                        }
+
                     }
 
                 }
--- src/main/java/com/zklt/parsing/model/entity/SpaceWeatherMonitoringDgd.java
+++ src/main/java/com/zklt/parsing/model/entity/SpaceWeatherMonitoringDgd.java
@@ -25,142 +25,15 @@ public class SpaceWeatherMonitoringDgd extends Message{
      */
     public String day;
 
-    /**
-     * 中纬度A指数
-     */
-    public String middleLatitudeA;
+
+    public String hour;
+
+    public String stationcode;
 
     /**
-     * 中纬度 第一个K指数
+     * k指数
      */
-    public String middleLatitudeKindice1;
-
-    /**
-     * 中纬度 第二个K指数
-     */
-    public String middleLatitudeKindice2;
-
-    /**
-     * 中纬度 第3个K指数
-     */
-    public String middleLatitudeKindice3;
-
-    /**
-     * 中纬度 第4个K指数
-     */
-    public String middleLatitudeKindice4;
-
-    /**
-     * 中纬度 第5个K指数
-     */
-    public String middleLatitudeKindice5;
-
-    /**
-     * 中纬度 第6个K指数
-     */
-    public String middleLatitudeKindice6;
-
-    /**
-     * 中纬度 第7个K指数
-     */
-    public String middleLatitudeKindice7;
-
-    /**
-     * 中纬度 第8个K指数
-     */
-    public String middleLatitudeKindice8;
-
-    /**
-     * 高纬度 A指数
-     */
-    public String highLatitudeA;
-
-    /**
-     * 高纬度 第1个K指数
-     */
-    public String highLatitudeKindice1;
-
-    /**
-     * 高纬度 2个K指数
-     */
-    public String highLatitudeKindice2;
-
-    /**
-     * 高纬度 第3个K指数
-     */
-    public String highLatitudeKindice3;
-
-    /**
-     * 高纬度 第4个K指数
-     */
-    public String highLatitudeKindice4;
-
-    /**
-     * 高纬度 第5个K指数
-     */
-    public String highLatitudeKindice5;
-
-    /**
-     * 高纬度 第6个K指数
-     */
-    public String highLatitudeKindice6;
-
-    /**
-     * 高纬度 第7个K指数
-     */
-    public String highLatitudeKindice7;
-
-    /**
-     * 高纬度 第8个K指数
-     */
-    public String highLatitudeKindice8;
-
-    /**
-     * 估计值 A指数
-     */
-    public String hestimatedA;
-
-
-    /**
-     * 估计值 第1个k指数
-     */
-    public String hestimatedKindice1;
-
-    /**
-     * 估计值 第2个k指数
-     */
-    public String hestimatedKindice2;
-
-    /**
-     * 估计值 第3个k指数
-     */
-    public String hestimatedKindice3;
-
-    /**
-     * 估计值 第4个k指数
-     */
-    public String hestimatedKindice4;
-
-    /**
-     * 估计值 第5个k指数
-     */
-    public String hestimatedKindice5;
-
-    /**
-     * 估计值 第6个k指数
-     */
-    public String hestimatedKindice6;
-
-    /**
-     * 估计值 第7个k指数
-     */
-    public String hestimatedKindice7;
-
-    /**
-     * 估计值 第8个k指数
-     */
-    public String hestimatedKindice8;
-
+    public String kindice;
 
 
 
